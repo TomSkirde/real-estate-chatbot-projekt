89be5f1db7a318ab752243b397fcf237f6bbb7a0
tom <skirdetom@gmail.com>
2025-12-07 23:14:35 +0100

    abstract repository

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,56 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { AbstractEntity } from './abstract.entity';
+import {
+  EntityManager,
+  Repository,
+  FindOptionsWhere,
+  QueryDeepPartialEntity,
+} from 'typeorm';
+
+export abstract class AbstractRepository<T extends AbstractEntity<T>> {
+  protected abstract readonly logger: Logger;
+
+  constructor(
+    private readonly entityRepository: Repository<T>,
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async create(entity: T): Promise<T> {
+    return this.entityManager.save(entity);
+  }
+
+  async findOne(where: FindOptionsWhere<T>): Promise<T> {
+    const entity = await this.entityRepository.findOne({ where });
+
+    if (!entity) {
+      this.logger.warn('Entity was not found with where', where);
+      throw new NotFoundException('Document was not found');
+    }
+
+    return entity;
+  }
+
+  async findOneAndUpdate(
+    where: FindOptionsWhere<T>,
+    partialEntity: QueryDeepPartialEntity<T>,
+  ) {
+    const updateResult = await this.entityRepository.update(
+      where,
+      partialEntity,
+    );
+    if (!updateResult.affected) {
+      this.logger.warn('Entity was not found with where', where);
+      throw new NotFoundException('Document was not found');
+    }
+
+    return this.findOne(where);
+  }
+
+  async find(where: FindOptionsWhere<T>) {
+    return this.entityRepository.findBy(where);
+  }
+
+  async findOneAndDelete(where: FindOptionsWhere<T>) {
+    return this.entityRepository.delete(where);
+  }
+}
